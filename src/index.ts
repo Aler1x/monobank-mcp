944a8303f8736c489849e849be10db9caf708fd1
Aler1x <95098796+Aler1x@users.noreply.github.com>
2025-08-02 14:27:59 +0300

    [refactor] update import paths in index.ts to remove file extensions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Server } from "@modelcontextprotocol/sdk/server/index.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { Server } from "@modelcontextprotocol/sdk/server/index";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
 import {
   CallToolRequestSchema,
   ListToolsRequestSchema,
   CallToolRequest,
-} from "@modelcontextprotocol/sdk/types.js";
+} from "@modelcontextprotocol/sdk/types";
 import { z } from "zod";
 
 // Types
